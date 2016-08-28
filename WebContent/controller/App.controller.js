3c2e0ca1e2b709ef847da9a856cbf76338cc70db
David Conmy <d.conmy1@gmail.com>
2016-08-28 01:38:14 +0100

    Added integration with JSON Model

--- WebContent/controller/App.controller.js
+++ WebContent/controller/App.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
-	return controller.extend("sap.ui.demo.wt.controller.App", {
+	return Controller.extend("sap.ui.demo.wt.controller.App", {
+
+		onInit: function () {
+			// set data model on view.
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 
 		onShowHello: function () {
+			// Show Hello World native JS alert.
 			alert("Hello World");
 		}
 	});
