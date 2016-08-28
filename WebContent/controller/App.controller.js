af89dddd70417bb7f636b5588d9a5d8241fb3bb1
David Conmy <d.conmy1@gmail.com>
2016-08-28 16:58:22 +0100

    Introduced i18n and referenced it in the helloWorld messages

--- WebContent/controller/App.controller.js
+++ WebContent/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageToast"
-], function (Controller, JSONModel, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/resource/ResourceModel"
+], function (Controller, JSONModel, MessageToast, ResourceModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.wt.controller.App", {
@@ -16,11 +17,23 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+
+			// Set i18n model on the view.
+			var i18nModel = new ResourceModel({
+				bundleName: "sap.ui.demo.wt.i18n.i18n"
+			});
+			this.getView().setModel(i18nModel, "i18n");
+
+
 		},
 
 		onShowHello: function () {
-			// Show Hello World with MessageToast.
-			MessageToast.show("Hello World");
+			// Read message from i18n model.
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			// Show the message.
+			MessageToast.show(sMsg);
 		}
 	});
 });
