0e1cb9cc8c70bea9e038a879a15e227eb4dc7c4c
David Conmy <d.conmy1@gmail.com>
2016-08-28 17:56:57 +0100

    Introduced Component.js and altered files for new layout

--- WebContent/controller/App.controller.js
+++ WebContent/controller/App.controller.js
@@ -1,33 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageToast",
-	"sap/ui/model/resource/ResourceModel"
-], function (Controller, JSONModel, MessageToast, ResourceModel) {
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.wt.controller.App", {
-
-		onInit: function () {
-			// set data model on view.
-			var oData = {
-				recipient: {
-					name: "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel);
-
-			// Set i18n model on the view.
-			var i18nModel = new ResourceModel({
-				bundleName: "sap.ui.demo.wt.i18n.i18n"
-			});
-			this.getView().setModel(i18nModel, "i18n");
-
-
-		},
-
-		onShowHello: function () {
+		onShowHello : function () {
 			// Read message from i18n model.
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
